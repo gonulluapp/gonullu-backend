7958e7b000ede9abba708a1f514a316e91befaec
erenyenigul <tc.eren.yenigul@gmail.com>
2023-02-08 15:46:44 +0300

    fix: get applications.

--- routes/apply.js
+++ routes/apply.js
@@ -5,31 +5,36 @@ const auth = require("../middlewares/auth");
 const Application = require("../models/apply");
 
 router.get("/", auth, async (req, res) => {
-  try {
-    const applications = await Application.find({
-    //   "post.user": req.user._id,
-    //   'post.isDeleted': false,
-    });
-    res.send(applications);
-  } catch (err) {
-    res.status(500).send(err);
-  }
+    try {
+        const post = await Post.find({
+            user: req.user._id,
+            isDeleted: false,
+        });
+
+        const applications = await Application.find({
+            post: post.map((p) => p._id),
+        });
+
+        res.send(applications);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 router.post("/", async (req, res) => {
-  const { name, telephoneNumber, supplyItems, postId } = req.body;
-  try {
-    const application = await Application.create({
-      name,
-      telephoneNumber,
-      supplyItems,
-      post: postId,
-    });
+    const { name, telephoneNumber, supplyItems, postId } = req.body;
+    try {
+        const application = await Application.create({
+            name,
+            telephoneNumber,
+            supplyItems,
+            post: postId,
+        });
 
-    res.status(201).send(application);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send();
-  }
+        res.status(201).send(application);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send();
+    }
 });
 
 module.exports = router;
